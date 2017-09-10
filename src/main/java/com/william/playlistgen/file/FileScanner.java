afcf907b8874c18d5290cfabf502781f9ccce7c9
William Heavin <william.james.heavin@gmail.com>
2017-09-10 12:21:42 +0100

    Refactored FileScanner class

--- src/main/java/com/william/playlistgen/file/FileScanner.java
+++ src/main/java/com/william/playlistgen/file/FileScanner.java
@@ -17,45 +17,50 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
  * @author William
  */
 public class FileScanner implements FilenameFilter {
 
     private static final String CODEC_MP3 = ".mp3";
+    private static final int LENGTH_OF_WHITESPACE = 41;
 
     @Override
-    public boolean accept(File dir, String name) {
+    public boolean accept(final File dir, final String name) {
         return name.endsWith(CODEC_MP3);
     }
 
     public List<Song> getMp3SongList(final File directory) {
-        File[] fileList = directory.listFiles();
-        List<Song> mp3SongList = new ArrayList<>();
+        final File[] fileList = directory.listFiles();
+        final List<Song> mp3SongList = new ArrayList<>();
 
         if (fileList == null) {
             return mp3SongList;
         }
-        for (File file : fileList) {
-            if (file.isDirectory()) {
-                List<Song> songsInDirectory = getMp3SongList(file);
-                mp3SongList.addAll(songsInDirectory);
-            } else if (file.getName().endsWith(CODEC_MP3)) {
-                System.out.print("Found file " + file.getName() + "                                         \r");
-                try {
-                    addSongDetailsFromFile(file, mp3SongList);
-                } catch (MalformedURLException ex) {
-                    Logger.getLogger(FileScanner.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
+        for (final File file : fileList) {
+            processFile(mp3SongList, file);
         }
         System.out.println("Found " + mp3SongList.size() + " files in directory " + directory.getPath() + "\n");
         return mp3SongList;
     }
 
-    private void addSongDetailsFromFile(File file, List<Song> mp3SongList) throws MalformedURLException {
-        Mp3TagFinder mp3TagFinder = new Mp3TagFinder(file);
-        Song song = new Song();
+    private void processFile(final List<Song> mp3SongList, final File file) {
+        if (file.isDirectory()) {
+            final List<Song> songsInDirectory = getMp3SongList(file);
+            mp3SongList.addAll(songsInDirectory);
+        } else if (file.getName().endsWith(CODEC_MP3)) {
+            System.out.println("Found file" + file.getName()
+                    + String.format("%" + LENGTH_OF_WHITESPACE + "s", "") + "\r");
+            try {
+                addSongDetailsFromFile(file, mp3SongList);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(FileScanner.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+
+    private void addSongDetailsFromFile(final File file, final List<Song> mp3SongList) throws MalformedURLException {
+        final Mp3TagFinder mp3TagFinder = new Mp3TagFinder(file);
+        final Song song = new Song();
         song.setTitle(mp3TagFinder.getSongTitle());
         song.setArtist(mp3TagFinder.getArtist());
         song.setAlbum(mp3TagFinder.getAlbum());
